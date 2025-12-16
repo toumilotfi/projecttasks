bb01a084ff5ef85b730701dc3392d5f0f3e13d1d
lotfi <lotfitoumi56@gmail.com>
2025-12-16 16:07:52 +0100

    test

---
+++ spring boot backend/src/main/java/com/SpringBoot/demo/SpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApplication.class, args);
+	}
+
+}
